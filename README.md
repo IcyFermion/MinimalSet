89dfcb999228c81f3e20d028f8c53a7a3532e1f2
Bingran Shen <shenbingran@gmail.com>
2024-07-03 16:32:48 -0400

    co-dependency feature

--- README.md
+++ README.md
@@ -4,20 +4,24 @@ Import the moduel from file:
 from minimal_set import MinimalSetCalc
 ```
 
-Initialize the calculation class with ``MinimalSetCalc(X, y, model=RandomForestRegressor(random_state=42, n_jobs=1), cpu_cores=1, num_iterations=100,feature_keep_rate=0.5, ranking_metric='feature_importances_')``
+Initialize the calculation class with ``MinimalSetCalc(X, y, model=RandomForestRegressor(random_state=42, n_jobs=1), cpu_cores=1, num_iterations=100,feature_keep_rate=0.5, ranking_metric='feature_importances_', cv_fold=5, output_dir='./output', size_threshold=10)``
 
 Parameters:
 
 - `X`: array-like, sparse matrix of shape (n_samples, n_features). The data to fit. Can be for example a list, or an array. **Required**.
-- `y`: array-like of shape (n_samples,) or (n_samples, n_outputs). The target variable to try to predict in the case of supervised learning. **Required**.
-- `mode`: learning model object implementing ‘fit’, with feature importance metrics, the object to use to fit the data. **Optional**.
+- `y`: array-like of shape (n_samples,) or (n_outputs, n_samples). The target variable to try to predict in the case of supervised learning. **Required**.
+- `target_names`: array-like of shape (n_outputs,). List of target names, for correct output path formatting. **Required**.
+- `model`: learning model object implementing ‘fit’, with feature importance metrics, the object to use to fit the data. **Optional**.
 - `cpu_cores`: int, number of CPU cores for parallel processing. default = 1. Use -1 for all available CPU cores. **Optional**.
 - `num_iterations`: int, number of random cross validation permutations to produce different minimal set results. **Optional**.
 - `feature_keep_rate`: float within range 0 to 1 (non inclusive). The feature keep rate at each feature reduction step, 0.2 means 20 percent of the feature will be kept each time the algorithm tries to reduce the number of features. **Optional**.
 - `ranking_metric`: str, attribute name of the feature importance metric in a fitted learning model. **Optional**.
 - `cv_fold`: int, number of cross validation fold for each permutation, default = 5. **Optional**.
+- `output_dir`: str, output directory, default = './output'. **Optional**.
+- `size_threshold`: int, size threshold for co-dependency analysis based on minimal set calculations, minimal set smaller than this number will be filtered out, default=10. **Optional**.
 
-Execute the calculation with ``execute(output_dir)`` method, default output directory is the current path. Minimal sets from each run will be output into its own csv file.
+
+Execute the calculation with ``execute(co_dependency_calc=True)`` method, use the boolean parameter `co_dependency_calc` to control weather or not to follow up the minimal set calculation with additional co-dependency analysis. By default, a follow-up co-dependency calculation will be applied to all minimal sets below the size threshold, and the results will be saved in `co_dependency/co_dependency.csv` in the output directory. A table of filtered minimal sets will be returned.
 
 
 
--- minimal_set.py
+++ minimal_set.py
@@ -1,36 +1,55 @@
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+import copy
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import cross_validate
 from sklearn.model_selection import KFold
 from functools import reduce
 from multiprocessing import Pool, cpu_count
+from pathlib import Path
+from itertools import combinations
 
 
 
 class MinimalSetCalc:
     def __init__(self, 
-                 X, 
-                 y, 
-                 model=RandomForestRegressor(random_state=42, n_jobs=1), 
-                 cpu_cores=1, 
-                 num_iterations=100, 
-                 feature_keep_rate=0.5,
-                 ranking_metric='feature_importances_',
-                 cv_fold=5):
+                X, 
+                y,
+                target_names, 
+                model=RandomForestRegressor(random_state=42, n_jobs=1), 
+                cpu_cores=1, 
+                num_iterations=100, 
+                feature_keep_rate=0.5,
+                ranking_metric='feature_importances_',
+                cv_fold=5,
+                output_dir='./output',
+                size_threshold=10):
         self.X = X
-        self.y = y
+        self.y_list = y
+        if len(y.shape) == 1:
+            self.y_list = np.array([y])
+        self.target_names = target_names
         self.model = model
+        self.size_threshold = size_threshold
         if (cpu_cores < 0): self.cpu_cores = cpu_count()
         else: self.cpu_cores = cpu_cores
         self.num_iterations = num_iterations
         self.feature_keep_rate = feature_keep_rate
         self.ranking_metric = ranking_metric
         self.cv_fold = cv_fold
+        self.output_dir = output_dir
+        for name in target_names:
+            Path(output_dir+'/'+name).mkdir(parents=True, exist_ok=True)
+            Path(output_dir+'/'+name+'/co_dependency').mkdir(parents=True, exist_ok=True)
+            Path(output_dir+'/'+name+'/minimal_sets').mkdir(parents=True, exist_ok=True)
+
     
     def minimal_set_calc(self, seed):
-        print('minimal sets calculation for seed: {}.............'.format(seed))
+        # result_df = pd.read_csv('{}/{}/minimal_sets/minimal_set_out_{}.csv'.format(self.output_dir, self.current_target, seed))
+        # filtered_df = result_df[result_df['minimal_set_acc'] > 0]
+        # filtered_df = filtered_df[filtered_df['minimal_set_size'] < self.size_threshold]
+        # return filtered_df
         X = self.X
         y = self.y
         feature_keep_rate = self.feature_keep_rate
@@ -39,7 +58,7 @@ class MinimalSetCalc:
         cv_out = cross_validate(self.model, X, y, cv=cv_splits, n_jobs=1, return_train_score=True, return_estimator=True)
         base_score = np.mean(cv_out['train_score'])
         feature_list = X.columns
-        current_feature_importance = reduce(lambda a, b: a + getattr(b, self.ranking_metric), cv_out['estimator'], 0)
+        current_feature_importance = reduce(lambda a, b: a + getattr(b, ranking_metric), cv_out['estimator'], 0)
         current_score = base_score
         current_test_score = base_score
         continue_flag = True
@@ -53,7 +72,7 @@ class MinimalSetCalc:
                 continue_flag = False
                 break
             feature_list = kept_features
-            current_feature_importance = reduce(lambda a, b: a + getattr(b, self.ranking_metric), current_cv_out['estimator'], 0)
+            current_feature_importance = reduce(lambda a, b: a + getattr(b, ranking_metric), current_cv_out['estimator'], 0)
             current_test_score = np.mean(current_cv_out['test_score'])
             if (len(feature_list) < 4):
                 continue_flag = False
@@ -72,7 +91,7 @@ class MinimalSetCalc:
             current_cv_out = cross_validate(self.model, current_X, y, cv=cv_splits, n_jobs=1, return_train_score=True, return_estimator=True)
             current_score = np.mean(current_cv_out['train_score'])
             minimal_set_test_score = np.mean(current_cv_out['test_score'])
-            current_feature_importance = reduce(lambda a, b: a + getattr(b, self.ranking_metric), current_cv_out['estimator'], 0)
+            current_feature_importance = reduce(lambda a, b: a + getattr(b, ranking_metric), current_cv_out['estimator'], 0)
             if (current_score < base_score):
                 continue_flag = False
                 minimal_set_test_score = 0
@@ -90,7 +109,7 @@ class MinimalSetCalc:
                     continue_flag = False
                     break
                 minimal_features = kept_features
-                current_feature_importance = reduce(lambda a, b: a + getattr(b, self.ranking_metric), current_cv_out['estimator'], 0)
+                current_feature_importance = reduce(lambda a, b: a + getattr(b, ranking_metric), current_cv_out['estimator'], 0)
                 minimal_set_test_score = np.mean(current_cv_out['test_score'])
                 if (len(minimal_features) < 4):
                     continue_flag = False
@@ -102,27 +121,79 @@ class MinimalSetCalc:
             minimal_features_importance_list.append('; '.join(str(v) for v in current_feature_importance/self.cv_fold))
             lef_over_features = lef_over_features.difference(minimal_features)
 
-        res = np.array([
-            len(feature_list),
-            ', '.join(str(v) for v in minimal_features_length_list),
-            ', '.join(str(v) for v in minimal_features_rmse_list),
-            ', '.join(str(v) for v in minimal_features_idx_list),
-            ', '.join(str(v) for v in minimal_features_importance_list),
-        ])
+        result_df = pd.DataFrame(index=range(len(minimal_features_length_list)))
+        result_df['minimal_set_size'] = minimal_features_length_list
+        result_df['minimal_set_idx'] = minimal_features_idx_list
+        result_df['minimal_set_acc'] = minimal_features_rmse_list
+        result_df['minimal_set_importances'] = minimal_features_importance_list
+        result_df.to_csv('{}/{}/minimal_sets/minimal_set_out_{}.csv'.format(self.output_dir, self.current_target, seed), index=False)
 
-        return res
+        filtered_df = result_df[result_df['minimal_set_acc'] > 0]
+        filtered_df = filtered_df[filtered_df['minimal_set_size'] < self.size_threshold]
+        return filtered_df
     
-    def execute(self, output_dir='.'):
-        with Pool(self.cpu_cores) as p:
-            result_list = list(tqdm(p.imap(self.minimal_set_calc, range(self.num_iterations)), total=self.num_iterations))
-        result_df_list = []
-        for result, i in zip(result_list, range(self.num_iterations)):
-            result_df = pd.DataFrame(index=range(len(result[1].split(', '))))
-            result_df['minimal_set_size'] = [int(j) for j in result[1].split(', ')]
-            result_df['minimal_set_idx'] = result[3].split(', ')
-            result_df['minimal_set_acc'] = [float(j) for j in result[2].split(', ')]
-            result_df['minimal_set_importances'] = result[4].split(', ')
-            result_df_list.append(result_df)
-            result_df.to_csv('{}/minimal_set_out_{}.csv'.format(output_dir, i), index=False)
-        return result_df_list
+    def co_dependency_calc(self, filtered_df, target_name):
+        # co dependency calculation
+        feature_names = list(self.X.columns)
+        co_appearance_dict = {}
+        solo_appearance_dict = {}
+        for minimal_set_idx in filtered_df['minimal_set_idx']:
+            minimal_set = [int(idx) for idx in minimal_set_idx.split('; ')]
+            minimal_set.sort()
+            for i, j in combinations(minimal_set, 2):  
+                if i in solo_appearance_dict: 
+                    solo_appearance_dict[i] += 1
+                else:
+                    solo_appearance_dict[i] = 1
+                if j in solo_appearance_dict: 
+                    solo_appearance_dict[j] += 1
+                else:
+                    solo_appearance_dict[j] = 1
+                if i in co_appearance_dict:
+                    if j in co_appearance_dict[i]:
+                        co_appearance_dict[i][j] += 1
+                    else:
+                        co_appearance_dict[i][j] = 1
+                else:
+                    co_appearance_dict[i] = {}
+                    co_appearance_dict[i][j] = 1
+        
+        # output into a dataframe
+        co_dependency_dict = copy.deepcopy(co_appearance_dict)
+        co_dependency_name_list = []
+        co_dependency_value_list = []
+        co_dependency_count_list = []
+
+        for i in co_appearance_dict:
+            for j in co_appearance_dict[i]:
+                total_appearance = 0.0+solo_appearance_dict[i]+solo_appearance_dict[j]-co_appearance_dict[i][j]
+                co_dependency_dict[i][j] = co_appearance_dict[i][j] / total_appearance
+                co_dependency_name_list.append(feature_names[i] + '; ' + feature_names[j])
+                co_dependency_value_list.append(co_dependency_dict[i][j])
+                co_dependency_count_list.append(total_appearance)
+
+        output_df = pd.DataFrame(index=co_dependency_name_list)
+        output_df['codependency'] = co_dependency_value_list
+        output_df['total_appearance'] = co_dependency_count_list
+        output_df = output_df.sort_values('codependency', ascending=False)
+
+        output_df.to_csv(self.output_dir+'/'+target_name+'/co_dependency/co_dependency.csv')
+
+    def execute(self, co_dependency_calc=True):
+        feature_names = pd.DataFrame(index=self.X.columns)
+        feature_names.to_csv(self.output_dir+'/feature_names.tsv', header=False)
+        filtered_df_list = []
+        for target_name, y in zip(self.target_names, self.y_list):
+            self.y = y
+            self.current_target = target_name
+            print('Minimal Set calculation for {}... ...'.format(target_name))
+            with Pool(self.cpu_cores) as p:
+                result_list = list(tqdm(p.imap(self.minimal_set_calc, range(self.num_iterations)), total=self.num_iterations))
+            filtered_df = pd.concat(result_list, ignore_index=True)
+            filtered_df_list.append(filtered_df)
+            filtered_df.to_csv(self.output_dir+'/'+target_name+'/filtered_minimal_sets.csv')
+        if co_dependency_calc:
+            for target_name, filtered_df in zip(self.target_names, filtered_df_list):
+                self.co_dependency_calc(filtered_df, target_name=target_name)
+        return filtered_df_list
 
